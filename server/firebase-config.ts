a952b9bff121c88a5fa83fcb4df1700f3a287a3f
AryanKumar1401 <63923329+AryanKumar1401@users.noreply.github.com>
2024-07-12 14:10:42 +0530

    Update firebase-config.ts

--- server/firebase-config.ts
+++ server/firebase-config.ts
@@ -10,13 +10,7 @@ import withFirebaseAuth from "react-with-firebase-auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC4-2S3K3bTKwe2TZSO91S_MLmbAff4u5E",
-  authDomain: "infostream-dti2023.firebaseapp.com",
-  projectId: "infostream-dti2023",
-  storageBucket: "infostream-dti2023.appspot.com",
-  messagingSenderId: "1090629776272",
-  appId: "1:1090629776272:web:be642f3bde0b90973c3915",
-  measurementId: "G-7Q11QK5CGX"
+
 };
 
 export const app = initializeApp(firebaseConfig);
